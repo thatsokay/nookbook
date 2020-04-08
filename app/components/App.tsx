25bb3ab8d08da4bbcc94d3c8d5d1d5de31abd10f
Andre Wong <andre@andre-wong.com>
2020-04-08 18:44:38 +1000

    Prettier options

--- app/components/App.tsx
+++ app/components/App.tsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React from 'react'
 
-import fish from "../../public/fish.json";
+import fish from '../../public/fish.json'
 
 const App = () => {
-  console.log(fish.length);
   return (
     <>
       <h1>Animal Crossing</h1>
-      {fish.map(({ name }) => (
+      {fish.map(({name}) => (
         <p>{name}</p>
       ))}
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
