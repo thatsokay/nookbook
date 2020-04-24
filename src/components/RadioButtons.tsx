bcfee88bffdae5e1da31cded6143fae2afcacb5e
Andre Wong <andre@andre-wong.com>
2020-04-24 22:07:58 +1000

    Memoise radio button label

--- src/components/RadioButtons.tsx
+++ src/components/RadioButtons.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useMemo} from 'react'
 import {Box, Typography, ButtonGroup, Button} from '@material-ui/core'
 
 interface Props<T> {
@@ -12,35 +12,39 @@ interface Props<T> {
 }
 
 // `extends unknown` is needed so TS doesn't think it's JSX
-const RadioButtons = <T extends unknown>(props: Props<T>) => (
-  <Box>
-    {(() => {
-      switch (typeof props.label) {
-        case 'undefined':
-          return <></>
-        case 'string':
-          return (
-            <Typography variant="button" component="p">
-              {props.label}
-            </Typography>
-          )
-        default:
-          return props.label
-      }
-    })()}
-    <ButtonGroup variant="contained" size="small">
-      {props.options.map((option) => (
-        <Button
-          onClick={() => props.onChange(option.value)}
-          color={props.selected === option.value ? 'primary' : 'default'}
-          disableElevation
-          key={`${option.name}-${option.value}`}
-        >
-          {option.name}
-        </Button>
-      ))}
-    </ButtonGroup>
-  </Box>
-)
+const RadioButtons = <T extends unknown>(props: Props<T>) => {
+  const label = useMemo(() => {
+    switch (typeof props.label) {
+      case 'undefined':
+        return <></>
+      case 'string':
+        return (
+          <Typography variant="button" component="p">
+            {props.label}
+          </Typography>
+        )
+      default:
+        return props.label
+    }
+  }, [props.label])
+
+  return (
+    <Box>
+      {label}
+      <ButtonGroup variant="contained" size="small">
+        {props.options.map((option) => (
+          <Button
+            onClick={() => props.onChange(option.value)}
+            color={props.selected === option.value ? 'primary' : 'default'}
+            disableElevation
+            key={`${option.name}-${option.value}`}
+          >
+            {option.name}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </Box>
+  )
+}
 
 export default RadioButtons
